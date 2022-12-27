25674eceb593c7629cbb071050aeedfc250285fc
soks29 <s.jesus@karizma.ma>
2022-12-28 00:21:15 +0100

    [UPP] modification commentaire

--- scripts2.sh
+++ scripts2.sh
@@ -6,6 +6,7 @@ JOUR=$(date +%Y%m%d-+%H:%M:%S)
 #creation d'un fichier avec le jour, le mois et l'annee preceder par le mot fichier sur le bureau
 touch /home/karizma/Bureau/${JOUR}file.txt
 
+
 ###
 
 #pour lancer la création d'un fichier chaque -6heures on utilise la commande ci-dessous sur le fichier cron (crontab -e) :
